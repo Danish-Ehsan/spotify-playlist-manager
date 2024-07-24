c36f7c2fb3558e54a9a1ad4e01a3f32b7213a9d7
Danish <danish.ehsan321@gmail.com>
2024-07-24 15:32:48 -0400

    Update Vite config
    Set up server proxy for api requests and build directory path.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 6201,
+    proxy: {
+      "/api": {
+        target: "http://localhost:6200/api",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  build: {
+    outDir: "../server/public",
+  },
+});
