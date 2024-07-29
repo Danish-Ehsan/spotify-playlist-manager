857d4aaec4992d4aba08382377c42248dd147d02
Danish <danish.ehsan321@gmail.com>
2024-07-29 19:48:41 -0400

    Moved logo image to correct folder
    Logo image which was imported in React was incorrectly placed in the public folder. It is now in the assets folder.

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../../public/app-logo.png";
+import logo from "../assets/app-logo.png";
 import Nav from "./nav";
 
 export default function Header() {
